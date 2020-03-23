e49cceed6e14c48e9a4a6715cec89b427a326104
sheen4n <sheen.an.goh@gmail.com>
2020-03-23 12:05:12 +0800

    Fixed Syntax Error

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import seedColors from './seedColors';
 import Palette from './Palette';
@@ -7,7 +6,7 @@ import Palette from './Palette';
 function App() {
   return (
     <div className="App">
-      <Palette palette={...seedColors[4]} />
+      <Palette {...seedColors[4]} />
     </div>
   );
 }
