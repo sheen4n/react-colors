99508ad7a78163a075a4ea04cbd972f782195d34
sheen4n <sheen.an.goh@gmail.com>
2020-03-23 16:19:37 +0800

    Add Navbar and Google Font

---
+++ src/Navbar.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Slider from 'rc-slider';
+
+import 'rc-slider/assets/index.css';
+import './Navbar.css';
+
+const Navbar = ({ level, changeLevel }) => {
+  return (
+    <header className="Navbar">
+      <div className="logo">
+        <a href="">ReactColorPicker</a>
+      </div>
+      <div className="slider-container">
+        <span>Level : {level}</span>
+        <div className="slider">
+          <Slider
+            defaultValue={level}
+            min={100}
+            max={900}
+            step={100}
+            onAfterChange={changeLevel}
+          />
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
--- src/Palette.js
+++ src/Palette.js
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
 import ColorBox from './ColorBox';
-import 'rc-slider/assets/index.css';
 import './Palette.css';
-import Slider from 'rc-slider';
+import Navbar from './Navbar';
 
 const Palette = ({ palette, ...rest }) => {
   const [level, setLevel] = useState(500);
@@ -11,20 +10,9 @@ const Palette = ({ palette, ...rest }) => {
 
   const changeLevel = newLevel => setLevel(newLevel);
 
-  console.log(colors);
-
   return (
     <div className="Palette">
-      <div className="slider">
-        <Slider
-          defaultValue={level}
-          min={100}
-          max={900}
-          step={100}
-          onAfterChange={changeLevel}
-        />
-      </div>
-
+      <Navbar level={level} changeLevel={changeLevel} />
       <div className="Palette-colors">
         {colors[level].map(color => (
           <ColorBox {...color} key={color.name} />
