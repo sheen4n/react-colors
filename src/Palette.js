38acd62599b2ff3392950e60f0c5689b20ab07a9
sheen4n <sheen.an.goh@gmail.com>
2020-03-23 12:04:20 +0800

    Added Palette

---
+++ src/Palette.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Palette = props => {
+  return (
+    <div className="Palette">
+      {/* navbar */}
+      <div className="Palette-colors">{/* other colors */}</div>
+      {/* footer */}
+    </div>
+  );
+};
+
+export default Palette;
