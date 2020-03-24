17abbd00b811f8fd1eab34f9dfd3a12d9bcce58b
Goh Sheen An <sheen.an.goh@gmail.com>
2020-03-24 12:19:34 +0800

    Added React Router

--- src/App.js
+++ src/App.js
@@ -3,12 +3,24 @@ import './App.css';
 import seedColors from './seedColors';
 import Palette from './Palette';
 import generatePalette from './utils/colorHelper';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className='App'>
-      <Palette palette={generatePalette(seedColors[4])} />
-    </div>
+    <>
+      <Switch>
+        <Route exact path='/' render={() => <h1>Palette list goes here!</h1>} />
+        <Route
+          exact
+          path='/palette/:id'
+          render={() => <h1>Individual Palette</h1>}
+        />
+      </Switch>
+
+      {/* <div className='App'>
+        <Palette palette={generatePalette(seedColors[4])} />
+      </div> */}
+    </>
   );
 }
 
