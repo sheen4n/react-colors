8d84a12824e77ab7dac031041b79127e4dc16857
Goh Sheen An <sheen.an.goh@gmail.com>
2020-03-24 13:15:38 +0800

    Added Styles for Mini Palette

--- src/PaletteList.js
+++ src/PaletteList.js
@@ -1,17 +1,14 @@
 import React from 'react';
 import seedColors from './seedColors';
-import { Link } from 'react-router-dom';
+import MiniPalette from './MiniPalette';
 
 const PaletteList = () => {
   const palettes = seedColors;
-  console.log(palettes);
 
   return (
     <div>
       {palettes.map(palette => (
-        <p>
-          <Link to={`/palette/${palette.id}`}>{palette.paletteName}</Link>
-        </p>
+        <MiniPalette {...palette} />
       ))}
     </div>
   );
