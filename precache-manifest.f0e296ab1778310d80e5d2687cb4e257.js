ee5deb4d57e5a03fa6403f25ac6b9e24300d0e58
sheen4n <sheen.an.goh@gmail.com>
2020-03-26 17:39:10 +0800

    Updates

---
+++ precache-manifest.f0e296ab1778310d80e5d2687cb4e257.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6599020c9c31c748b6fbf50b09628850",
+    "url": "/react-colors/index.html"
+  },
+  {
+    "revision": "7625a16dc1c0b3089750",
+    "url": "/react-colors/static/css/2.7095f003.chunk.css"
+  },
+  {
+    "revision": "cd448641abfcbf246471",
+    "url": "/react-colors/static/css/main.f30aae92.chunk.css"
+  },
+  {
+    "revision": "7625a16dc1c0b3089750",
+    "url": "/react-colors/static/js/2.66b22779.chunk.js"
+  },
+  {
+    "revision": "5a98f98e29a6df7977c8f8d2444b57ac",
+    "url": "/react-colors/static/js/2.66b22779.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cd448641abfcbf246471",
+    "url": "/react-colors/static/js/main.e3437455.chunk.js"
+  },
+  {
+    "revision": "de3272b7ef08579daa59",
+    "url": "/react-colors/static/js/runtime-main.3df10e57.js"
+  },
+  {
+    "revision": "77428cd498bf82179ff99fdb246b2494",
+    "url": "/react-colors/static/media/bg.77428cd4.svg"
+  }
+]);
